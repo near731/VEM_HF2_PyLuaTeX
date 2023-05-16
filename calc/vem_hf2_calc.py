fb0f7c806f17fff062475e1d248d0fc9ef3d1ccf
near731 <nemeth.aaron1@gmail.com>
2023-05-16 14:01:19 +0200

    klaj.

--- calc/vem_hf2_calc.py
+++ calc/vem_hf2_calc.py
@@ -162,5 +162,165 @@ def calculate():
     eDOF3_b = [2*ecs_b[2,0]-1, 2*ecs_b[2,0], 2*ecs_b[2,1]-1, 2*ecs_b[2,1]]
 
     K_glob_b = sp.Matrix(ExtMatrix(K_1_b, eDOF1_b, 8) + ExtMatrix(K_2_b, eDOF2_b, 8)+ ExtMatrix(K_3_b, eDOF3_b, 8))
-    
+
+    #Tömeg mátrixok
+
+    #Elemként
+
+    M_1_b=M_i(L_1_b)
+    M_2_b=M_i(L_2_b)
+    M_3_b=M_i(L_3_b)
+
+    #Globális tömeg mátrix
+
+    M_b = sp.Matrix(ExtMatrix(M_1_b, eDOF1_b, 8) + ExtMatrix(M_2_b, eDOF2_b, 8)+ ExtMatrix(M_3_b, eDOF3_b, 8))
+
+    #Kondenzált mennyiségek meghatározása
+
+    #Kondenzált merevségi mátrix
+
+    freeDoF_b=sp.Matrix([[3,4,5,6,8]])
+
+    K_kond_b=SubMatrix(K_glob_b,freeDoF_b)
+
+    #Kondenzált tömeg mátrix
+
+    M_kond_b=SubMatrix(M_b,freeDoF_b)
+
+    #Sajátkörfrekvenciák meghatározása:
+
+    #Sajátkörfrekvenciák
+
+    α_b=alpha(K_kond_b,M_kond_b,4)
+    α_1_b=α_b[0]
+    α_2_b=α_b[1]
+    α_3_b=α_b[2]
+
+    #Sajátfrekvenciák
+
+    f_1_b=freq(α_1_b)
+    f_2_b=freq(α_2_b)
+    f_3_b=freq(α_3_b)
+
+    #4.feladat
+
+    #Rudak hosszai
+
+    L_1_c=L_1_b
+    L_2_c=L_2_b
+    L_3_c=L_3_b
+
+    #Merevségi mátrixok
+
+    K_glob_c=K_glob_b
+
+    #Tömeg mátrixok
+
+    M_k=sp.Matrix([[0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0],
+                [0,0,0,0,m_0,0,0,0],
+                [0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0]])
+
+    M_c=M_b+M_k
+
+    #Mennyiségek kondenzálása
+
+    freeDoF_c=freeDoF_b
+
+    #Kondenzált merevségi mátrix
+
+    K_kond_c=K_kond_b
+
+    #Kondenzált tömeg mátrix
+
+    M_kond_c=SubMatrix(M_c,freeDoF_c)
+
+    #Sajátkörfrekvenciák meghatározása:
+
+    #Sajátkörfrekvenciák
+
+    α_c=alpha(K_kond_c,M_kond_c,5)
+
+    α_1_c=α_c[0]
+    α_2_c=α_c[1]
+    α_3_c=α_c[2]
+
+    #Sajátfrekvenciák
+
+    f_1_c=freq(α_1_c)
+    f_2_c=freq(α_2_c)
+    f_3_c=freq(α_3_c)
+
+    #5.feladat
+
+    #Rudak hosszai
+
+    L_1_d=L_1_b
+    L_2_d=L_2_b
+    L_3_d=L_3_b
+
+    #Merevségi mátrixok
+
+    K_glob_d=K_glob_b
+
+    #Tömeg mátrixok
+
+    def M_e(L_i):
+        M_e=ρ*A*L_i/2*sp.Matrix([[1,0,0,0],
+                             [0,L_i**2/12,0,0],
+                             [0,0,1,0],
+                             [0,0,0,L_i**2/12]])
+        return M_e
+
+    #eDOFs
+
+    eDOF1_d=eDOF1_b
+    eDOF2_d=eDOF2_b
+    eDOF3_d=eDOF3_b
+
+    #Tömeg mátrixok
+
+    M_1_d=M_e(L_1_d)
+    M_2_d=M_e(L_2_d)
+    M_3_d=M_e(L_3_d)
+
+    M_d = sp.Matrix(ExtMatrix(M_1_d, eDOF1_d, 8) + ExtMatrix(M_2_d, eDOF2_d, 8)+ ExtMatrix(M_3_d, eDOF3_d, 8))
+    M_d+=M_k
+
+    #Mennyiségek kondenzálása
+
+    freeDoF_d=freeDoF_b
+
+    #Kondenzált merevségi mátrix
+
+    K_kond_d=K_kond_b
+
+    #Kondenzált tömeg mátrix
+
+    M_kond_d=SubMatrix(M_d,freeDoF_d)
+
+    #Sajátkörfrekvenciák meghatározása:
+
+    #Sajátkörfrekvenciák
+
+    α_d=alpha(K_kond_d,M_kond_d,5)
+    α_1_d=α_d[0]
+    α_2_d=α_d[1]
+    α_3_d=α_d[2]
+
+    #Sajátfrekvenciák
+
+    f_1_d=freq(α_1_d)
+    f_2_d=freq(α_2_d)
+    f_3_d=freq(α_3_d)
+
+    V={
+       "A":A,
+       "Iz":Iz, 
+    }
+
 
